b9ad0ee6a7f9733fa4428c0d1ed18d43bf59ee16
HoangVanLuong2207 <luonghvpp03220@gmail.com>
2025-09-08 11:40:18 +0700

    deployv12

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return "✅ Migrations executed successfully!";
 });
